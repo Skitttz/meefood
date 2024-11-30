388d4836b1461b1ae0bf6c33f59a398f38360411
SKitttz <skittz.dev@gmail.com>
2024-11-30 12:55:12 -0300

    refact(App): import providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import { RouteConfig } from "@routes/config";
 import { HelmetProvider, Helmet } from "react-helmet-async";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/theme/theme-provider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s - Meefood" />
-      <BrowserRouter>
-        <Toaster richColors />
-        <div>
-          <RouteConfig />
-        </div>
-      </BrowserRouter>
+      <ThemeProvider storageKey="meefood-themes" defaultTheme="system">
+        <Helmet titleTemplate="Meefood: %s" />
+        <BrowserRouter>
+          <Toaster richColors />
+          <QueryClientProvider client={queryClient}>
+            <RouteConfig />
+          </QueryClientProvider>
+        </BrowserRouter>
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
