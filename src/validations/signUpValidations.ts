68179208007b9bfdcb182771f581ccb3f20e6030
SKitttz <skittz.dev@gmail.com>
2024-12-01 00:24:39 -0300

    refact(validations): better validation sign-up

--- src/validations/signUpValidations.ts
+++ src/validations/signUpValidations.ts
@@ -1,10 +1,18 @@
-import {z} from "zod";
+import { z } from "zod";
 
 export const SignUpForm = z.object({
-  restaurantName: z.string(),
-  managerName: z.string(),
-  email: z.string().email(),
-  phone: z.string(),
-})
+  restaurantName: z
+    .string()
+    .min(1, { message: "Restaurant name is required." }) ,
+  managerName: z
+    .string()
+    .min(1, { message: "Manager name is required." }) ,
+  email: z
+    .string()
+    .email({ message: "Invalid email address." }),
+  phone: z
+    .string()
+    .min(10, { message: "Phone number must be at least 10 characters." }),
+});
 
-export type SignUpForm = z.infer<typeof SignUpForm>
+export type SignUpForm = z.infer<typeof SignUpForm>;
