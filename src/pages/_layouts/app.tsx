1139ca2684d190cdb6adaead8e861b99e840d7af
SKitttz <skittz.dev@gmail.com>
2024-12-01 00:29:52 -0300

    refact(pages): import AuthInterceptor and Header Auth

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,10 +1,12 @@
-import { Header } from "@/components/header";
+import { HeaderAuthenticated } from "@/components/header/header-auth";
+import { useAuthInterceptor } from "@/hooks/useAuthInterceptor";
 import { Outlet } from "react-router-dom";
 
 export function AppLayout() {
+  useAuthInterceptor();
   return (
     <div className="flex min-h-screen flex-col antialiased">
-      <Header />
+      <HeaderAuthenticated />
       <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
         <Outlet />
       </div>
