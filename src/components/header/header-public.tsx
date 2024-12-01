0e41519b71d9ff5a0deec1f2582aac3d5de83536
SKitttz <skittz.dev@gmail.com>
2024-12-01 00:30:30 -0300

    style(components): add in header public side image

--- src/components/header/header-public.tsx
+++ src/components/header/header-public.tsx
@@ -4,16 +4,18 @@ import { Link } from "react-router-dom";
 
 function HeaderPublic() {
   return (
-    <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+    <div className="border-foreground/5 text-muted-foreground bg-side-menu flex h-full flex-col justify-between border-r bg-cover p-10">
       <Link
         to={AppRoutesEnum.SIGN_IN}
-        className="flex items-center gap-2 text-lg font-medium text-foreground"
+        className="text-foreground flex items-center gap-2 text-lg font-medium"
       >
         <img src={MeefoodLogo} className="h-8 w-8" alt="Logo Meefood" />
         <span className="font-semibold">Meefood</span>
       </Link>
-      <footer className="text-sm">
-        Panel Partner &copy; Meefood - {new Date().getFullYear()}
+      <footer className="flex text-sm">
+        <p className="bg-secondary/80 rounded-md p-1 text-slate-200">
+          Panel Partner &copy; Meefood - {new Date().getFullYear()}
+        </p>
       </footer>
     </div>
   );
