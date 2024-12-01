6a4eccd435d9b9d8135fe67284ca7bb9e495b1ee
SKitttz <skittz.dev@gmail.com>
2024-12-01 00:21:37 -0300

    add new routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -14,5 +14,12 @@ export enum ApiRoutesEnum  {
   USER = "/me",
   MANAGED_RESTAURANT = "/managed-restaurant",
   PROFILE = "/profile",
-  SIGN_OUT = '/sign-out'
+  SIGN_OUT = '/sign-out',
+  ORDERS = '/orders',
+  DAY_ORDERS_AMOUNT = '/metrics/day-orders-amount',
+  MONTH_ORDERDS_AMOUNT ='/metrics/month-orders-amount',
+  MONTH_ORDERDS_CANCELED_AMOUNT ='/metrics/month-canceled-orders-amount',
+  MONTH_REVENUE = '/metrics/month-receipt',
+  POPULAR_PRODUCTS = '/metrics/popular-products',
+  DAILY_RECEIPT_IN_PERIOD = '/metrics/daily-receipt-in-period'
 }
