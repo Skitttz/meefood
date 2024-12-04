28127c0480aeb639239f211d1179f4c94acb9639
SKitttz <skittz.dev@gmail.com>
2024-12-03 21:54:26 -0300

    chore: config  vitest

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import path from "node:path";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { UserConfig } from "vite";
+import type { InlineConfig } from "vitest";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +13,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   }, 
-});
+  test:{ globals: true, setupFiles: ['./src/_tests_/config/setup.ts'], environment:'happy-dom'}, 
+  } as UserConfig & {
+    test: InlineConfig
+})
