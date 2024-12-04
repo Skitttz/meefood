8312db3af5a66f5214b2857fc5c896b3d07fb101
SKitttz <skittz.dev@gmail.com>
2024-12-03 21:55:10 -0300

    chore(components): insert html attr data-testid for test

--- src/components/ui/order-status.tsx
+++ src/components/ui/order-status.tsx
@@ -12,6 +12,7 @@ function OrderStatus({ statusOrder }: OrderStatusProps) {
   return (
     <div className="flex items-center gap-2">
       <span
+        data-testid="badge"
         className={COLORS_STATUS_ORDERS[ORDERS_CONFIG[statusOrder].status]}
       />{" "}
       <span className="font-medium text-muted-foreground">
