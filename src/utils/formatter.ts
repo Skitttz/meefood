d23d23bcf8e8da80c6cc57a13e51a7886fae36ef
SKitttz <skittz.dev@gmail.com>
2024-12-10 08:40:38 -0300

    refact(utils): create function formatPhoneNumber

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -4,3 +4,13 @@ export function formatterValueCurrency(value:number){
     currency: "USD",
   })
 }
+
+export function formatPhoneNumber(value: string) {
+  const phoneNumberFormated = value.replace(/\D/g, '');
+
+  return phoneNumberFormated
+    .replace(/^(\d{0,2})/, '($1') 
+    .replace(/(\(\d{2})(\d{0,5})/, '$1) $2') 
+    .replace(/(\d{5})(\d{0,4})/, '$1 $2') 
+    .trim(); 
+}
