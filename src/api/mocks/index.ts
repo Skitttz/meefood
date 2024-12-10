aa79a3f9712d3c76a41b147520015b6729ba3ad5
SKitttz <skittz.dev@gmail.com>
2024-12-10 08:37:51 -0300

    refac(api): insert  mocks in setupWorker msw

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,7 +1,8 @@
 import { env } from "@/env";
 import {setupWorker} from "msw/browser"
+import { signInMock, registerRestaurantMock, getDayOrdersAmountMock, getMonthOrdersAmountMock, getMonthCanceledOrdersAmountMock,getMonthRevenueMock,getDailyRevenueInPeriodMock, getManagedRestaurantMock ,getUserMock ,updateProfileMock, getPopularProductMock, getOrdersMock,getOrderDetailsMock } from "./allMocks";
 
-export const worker = setupWorker();
+export const worker = setupWorker(signInMock,registerRestaurantMock,getDayOrdersAmountMock,getMonthOrdersAmountMock,getMonthCanceledOrdersAmountMock,getMonthRevenueMock,getDailyRevenueInPeriodMock, getManagedRestaurantMock, getUserMock,getPopularProductMock, updateProfileMock, getOrdersMock,getOrderDetailsMock);
 const isEnvTest: boolean = env.MODE === 'test';
 
 export async function enableMSW(){
